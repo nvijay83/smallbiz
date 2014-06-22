db75e221e7616440994299719b42db78d96c452c
vijayakrishnan nagarajan <vijay@vijayakrishnans-MacBook-Pro.local>
2014-06-22 09:43:54 -0700

    converted HTTP put to post

--- restapi.py
+++ restapi.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 from flask.ext import restful
 import time
 from post import getposts, addpost, getfeed, likepost, dislikepost,addmsg,getmsg
+import operator
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -17,9 +18,19 @@ class Business(restful.Resource):
         '''
         print "b_id is %d"%b_id
         return getposts(b_id)
+    
+    def post(self, b_id):
+        print request
+	post = request.args.get('post')
+        #image = request.form['image']
+        t = time.time()
+        print "post is %s, %d"%(post, t)
+        addpost(b_id, post, '', 0, 0, t)
+        return 0
 
     def put(self, b_id):
-        post = request.form['post']
+        print request
+	post = request.form['post']
         #image = request.form['image']
         t = time.time()
         print "post is %s, %d"%(post, t)
@@ -29,13 +40,23 @@ class Business(restful.Resource):
 class Customer(restful.Resource):
     def get(self, c_id):
         print "customer id is %d", c_id
-        return getfeed(c_id)
+	l = getfeed(c_id)
+	l = reduce(lambda x,y: x+y,l)
+        return l
 
 
 
 class Posts(restful.Resource):
     def put(self, post_id):
-        like = request.form['like']
+        like = request.args.get('like')
+        print "post_id %d, like %d",(post_id, like)
+        if like == 'True':
+            likepost(post_id)
+        else:
+            dislikepost(post_id)
+
+    def post(self, post_id):
+        like = request.args.get('like')
         print "post_id %d, like %d",(post_id, like)
         if like == 'True':
             likepost(post_id)
@@ -49,6 +70,10 @@ class Messages(restful.Resource):
     def put(self, bid, cid):
         msg = request.form['message']
         addmsg(bid, cid, msg, time.time())
+    
+    def post(self, bid, cid):
+        msg = request.args.get('message')
+        addmsg(bid, cid, msg, time.time())
 
 
 
@@ -59,4 +84,4 @@ api.add_resource(Messages, '/message/<int:bid>/<int:cid>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
